6408d8131d54ea034d020059046799142aaaa2ef
Balya <alexander.balya@gmail.com>
2020-06-24 03:53:14 +0200

    Создана ORM структура данных

--- lib/synonyms.php
+++ lib/synonyms.php
@@ -1,6 +1,86 @@
 <?php
+
 namespace Neft\Synonyms;
 
-class Synonyms {
+use Bitrix\Main\Entity;
+use Bitrix\Main\ORM\Data\DataManager;
+use Bitrix\Main\ORM\Fields\FloatField;      // Число
+use Bitrix\Main\ORM\Fields\StringField;     // Строка
+use Bitrix\Main\ORM\Fields\DatetimeField;   // Дата/Время
+use Bitrix\Main\ORM\Fields\DateField;       // Дата
+use Bitrix\Main\ORM\Fields\IntegerField;    // Целое число
+use Bitrix\Main\ORM\Fields\TextField;       // Текст
+use Bitrix\Main\ORM\Fields\BooleanField;    // Да/Нет
+use Bitrix\Main\Type\DateTime;
+use Bitrix\Main\Localization\Loc;
 
+Loc::loadMessages(__FILE__);
+
+class SynonymsTable extends DataManager
+{
+  public static function getTableName()
+  {
+    return 'neft_synonyms';
+  }
+
+  /**
+   * Структура таблицы данных.
+   * @link https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=43&LESSON_ID=4803&LESSON_PATH=3913.5062.5748.4803
+   *
+   * @return void
+   */
+  public static function getMap()
+  {
+    return array(
+      new IntegerField('ID', array(
+        'title' => Loc::getMessage('NEFT_SYNONYMS_DB_TITLE_ID'),
+        'primary' => true,
+        'autocomplete' => true
+      )),
+
+      new StringField('WORD', array(
+        'title' => Loc::getMessage('NEFT_SYNONYMS_DB_TITLE_WORD'),
+        'required' => true,
+        'validation' => function () {
+          return array(
+              new Entity\Validator\Unique,
+          );
+        }
+      )),
+
+      new TextField('SYNONYMS', array(
+        'title' => Loc::getMessage('NEFT_SYNONYMS_DB_TITLE_SYNONYMS'),
+      )),
+
+      new IntegerField('SORT', array(
+        'title' => Loc::getMessage('NEFT_SYNONYMS_DB_TITLE_SORT'),
+        'default_value' => 100,
+      )),
+
+      new DatetimeField('UPDATED', array(
+        'title' => Loc::getMessage('NEFT_SYNONYMS_DB_TITLE_UPDATED'),
+        'default_value' => function () {
+          return new DateTime();
+        },
+      )),
+
+      new BooleanField('ACTIVE', array(
+        'title' => Loc::getMessage('NEFT_SYNONYMS_DB_TITLE_ACTIVE'),
+        'values' => array('N', 'Y'),
+        'default_value' => 'Y',
+      )),
+    );
+  }
+
+  public static function getUserId()
+  {
+    global $USER;
+    return $USER->GetID();
+  }
+
+  public static function update($primary, array $data)
+  {
+    $data['MODIFIED_BY'] = static::getUserId();
+    return parent::update($primary, $data);
+  }
 }
