ff4b2316c530534487861321263466353a5f81c8
Balya <alexander.balya@gmail.com>
2020-08-11 19:36:19 +0200

    Изменен адрес сайта партнера

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,8 +1,8 @@
 <?php
 $MESS["NEFT_SYNONYMS_MODULE_NAME"] = "Поисковые синонимы";
 $MESS["NEFT_SYNONYMS_MODULE_DESCRIPTION"] = "Модуль позволяет задавать синонимы для поисковых запросов.";
-$MESS["NEFT_SYNONYMS_PARTNER_NAME"] = "NEFT.digital";
-$MESS["NEFT_SYNONYMS_PARTNER_URI"] = "https://neft.digital";
+$MESS["NEFT_SYNONYMS_PARTNER_NAME"] = "NEFT";
+$MESS["NEFT_SYNONYMS_PARTNER_URI"] = "https://neftdigital.ru";
 $MESS["NEFT_SYNONYMS_INSTALL_TITLE"] = "Установка модуля";
 $MESS["NEFT_SYNONYMS_INSTALL_ERROR_VERSION"] = "Версия главного модуля ниже 14. Не поддерживается технология D7, необходимая модулю. Пожалуйста обновите систему.";
 $MESS["NEFT_SYNONYMS_UNINSTALL_TITLE"] = "Удаление модуля";
