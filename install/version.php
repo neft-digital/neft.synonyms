6f87b0e57852beea8b22c54dfdbc7c6f29c42404
Balya <alexander.balya@gmail.com>
2020-08-12 21:29:37 +0200

    Описание модуля перенесено в README.md

--- install/version.php
+++ install/version.php
@@ -1,5 +1,5 @@
 <?php
 $arModuleVersion = array(
-  "VERSION"       => "0.1.1",
-  "VERSION_DATE"  => "2020-08-07 01:40:00"
+  "VERSION"       => "0.1.2",
+  "VERSION_DATE"  => "2020-08-12 21:27:00"
 );
